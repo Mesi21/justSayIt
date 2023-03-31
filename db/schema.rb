b904862cb65fdf88991ee84062c4170069e12efa
Mesi <mse212002@gmail.com>
2023-03-31 09:28:55 +0300

    :construction: remove table user

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_160406) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.string "bio"
-    t.integer "posts_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
